f9069cc390ec88550628fe36f32c6c51962a2b88
Bryan Kneis <b.kneis2011@gmail.com>
2017-11-30 15:31:06 +0000

    Comment out parameter builder for now

---
+++ include/ParameterBuilder.h
@@ -0,0 +1,16 @@
+#ifndef STEREORECOGNITION_PARAMETERBUILDER_H
+#define STEREORECOGNITION_PARAMETERBUILDER_H
+
+
+//class ParameterBuilder {
+//
+//public:
+//
+//    void* get(std::string strategy) {
+//
+//    }
+//
+//};
+
+
+#endif //STEREORECOGNITION_PARAMETERBUILDER_H
