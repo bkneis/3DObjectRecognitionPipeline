e79efffd7b0f6add6a431652958b6f839a57a1d3
bkneis <b.kneis2011@gmail.com>
2017-12-18 09:12:50 +0000

    Add timers to log performance of cpu vs gpu

--- src/faceDetector.cpp
+++ src/faceDetector.cpp
@@ -18,6 +18,8 @@ CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 String window_name = "Capture - Face detection";
 
+Ptr<cuda::CascadeClassifier> cascade_gpu = cuda::CascadeClassifier::create("/home/arthur/face_cascade_cuda.xml");
+
 int main( int argc, const char** argv )
 {
     CommandLineParser parser(argc, argv,
@@ -64,47 +66,46 @@ void detectAndDisplayCPU( Mat frame )
 {
     std::vector<Rect> faces;
     Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
+    //cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    //equalizeHist( frame_gray, frame_gray );
     //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-    }
-    Mat resized;
-    resize(frame, resized, Size(), 0.5, 0.5);
-    //-- Show what you got
-
-    imshow( window_name, resized );
-
-    while (true) {
-        char c = (char)waitKey(10);
-        if( c == 27 ) { break; } // press escape
-    }
+    face_cascade.detectMultiScale( frame, faces, 1.3, 5, 0|CASCADE_SCALE_IMAGE, gst );
+//    for ( size_t i = 0; i < faces.size(); i++ )
+//    {
+//        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+//        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+//    }
+//    Mat resized;
+//    resize(frame, resized, Size(), 0.5, 0.5);
+//    //-- Show what you got
+//
+//    imshow( window_name, resized );
+//
+//    while (true) {
+//        char c = (char)waitKey(10);
+//        if( c == 27 ) { break; } // press escape
+//    }
 
 }
 
 void detectAndDisplayGPU( Mat frame )
 {
-    Ptr<cuda::CascadeClassifier> cascade_gpu = cuda::CascadeClassifier::create("/home/arthur/face_cascade_cuda.xml");
     cuda::GpuMat image_gpu(frame);
     cuda::GpuMat objbuf;
     cascade_gpu->detectMultiScale(image_gpu, objbuf);
     std::vector<Rect> faces;
     cascade_gpu->convert(objbuf, faces);
-    for (int i = 0; i < faces.size(); i++) {
-        cv::rectangle(frame, faces[i], Scalar(255));
-    }
-
-    Mat resized;
-    resize(frame, resized, Size(), 0.5, 0.5);
-    imshow( window_name, resized );
-
-    while (true) {
-        char c = (char)waitKey(10);
-        if( c == 27 ) { break; } // press escape
-    }
+//    for (int i = 0; i < faces.size(); i++) {
+//        cv::rectangle(frame, faces[i], Scalar(255));
+//    }
+//
+//    Mat resized;
+//    resize(frame, resized, Size(), 0.5, 0.5);
+//    imshow( window_name, resized );
+//
+//    while (true) {
+//        char c = (char)waitKey(10);
+//        if( c == 27 ) { break; } // press escape
+//    }
 
 }
