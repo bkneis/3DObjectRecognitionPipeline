7a4aaa4ebe29301d0aac3b10623653d42114f3d9
bkneis <b.kneis2011@gmail.com>
2018-01-05 17:13:58 +0000

    Rename classifier to use I for interface and add template args

--- include/classifier/KNN.h
+++ include/classifier/KNN.h
@@ -5,21 +5,22 @@
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/io/pcd_io.h>
 #include <utils.h>
-#include "Classifier.h"
+#include "IClassifier.h"
 
 
 namespace classifier {
 
-    class KNN : public Classifier<GlobalDescriptorsPtr> {
+    template <class FeatureT, class FeatureDescriptor, class FeatureDescriptorPtr>
+    class KNN : public IClassifier<FeatureT, FeatureDescriptorPtr> {
 
     public:
 
-        KNN() : Classifier() {}
+        KNN() {}
 
-        Subject<GlobalDescriptorT>*
-        classify(GlobalDescriptorsPtr subject) override
+        Subject<FeatureT>*
+        classify(FeatureDescriptorPtr subject) override
         {
-            const GlobalDescriptorT & query_descriptor = subject->points[0];
+            const FeatureT & query_descriptor = subject->points[0];
 
             std::vector<int> nn_index (1);
             std::vector<float> nn_sqr_distance (1);
@@ -27,21 +28,21 @@ namespace classifier {
             int i = nn_index[0];
             const int & best_match = nn_index[0];
 
-            Subject<GlobalDescriptorT>* test = (models_[best_match]);
+            Subject<FeatureT>* test = (models_[best_match]);
 
             return test;
         }
 
         void
-        populateDatabase(std::vector<Subject<GlobalDescriptorT>*> models) override
+        populateDatabase(std::vector<Subject<FeatureT>*> models) override
         {
             models_ = models;
             size_t n = models_.size ();
-            descriptors_ = GlobalDescriptorsPtr (new GlobalDescriptors);
+            descriptors_ = FeatureDescriptorPtr (new FeatureDescriptor);
             for (size_t i = 0; i < n; ++i) {
                 *descriptors_ += *(models_[i]->descriptor);
             }
-            kdtree_ = pcl::KdTreeFLANN<GlobalDescriptorT>::Ptr (new pcl::KdTreeFLANN<GlobalDescriptorT>);
+            kdtree_ = pcl::KdTreeFLANN<GlobalDescriptorT>::Ptr (new pcl::KdTreeFLANN<FeatureT>);
             kdtree_->setInputCloud (descriptors_);
         }
 
@@ -50,9 +51,9 @@ namespace classifier {
 
     private:
 
-        std::vector<Subject<GlobalDescriptorT>*> models_;
-        GlobalDescriptorsPtr descriptors_;
-        pcl::KdTreeFLANN<GlobalDescriptorT>::Ptr kdtree_;
+        std::vector<Subject<FeatureT>*> models_;
+        FeatureDescriptorPtr descriptors_;
+        typename pcl::KdTreeFLANN<FeatureT>::Ptr kdtree_;
 
     };
 
